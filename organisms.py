14452bde6b8fb8ca47775e8e43ea84d90c3f66f4
guilhermepata <35606946+guilhermepata@users.noreply.github.com>
2018-03-22 15:51:12 +0000

    File is now working

--- organisms.py
+++ organisms.py
@@ -4,6 +4,28 @@ Created on Wed Mar 21 19:30:31 2018
 
 @author: Guilherme Pata
 """
+
+from random import uniform
+from numpy import exp
+
+def sigmoid(x):
+    return 1/(1+exp(-100*x))-0.5
+
+def w_choice(seq):
+    total_prob = sum(item[1] for item in seq)
+    chosen = uniform(0, total_prob)
+    cumulative = 0
+    for item, probality in seq:
+        cumulative += probality
+        if cumulative > chosen:
+            return item
+        
+def randex(f,x,prob):
+    l=[(True,prob),(False,1-prob)]
+    var = w_choice(l)
+    if var:
+        return f(x)
+
 class org:
     def __init__(self,sex='m',gene1='0.5',age=0):
         self._sex = sex
@@ -16,6 +38,8 @@ class org:
         return self._gene1
     def age(self):
         return self._age
+    def show(self):
+        return (self._sex,self._gene1,self._age)
         
     def grow(l,y):
         for i in l:
@@ -27,6 +51,9 @@ class pop:
         
     def content(self):
         return self._content
+    def show(self):
+        for i in self._content:
+            print(i.show())
     def size(self):
         return len(self._content)
     def gene1pool(self):
@@ -42,10 +69,12 @@ class pop:
     def sex_num_a1(self,a,sex):
         return len([i for i in self._content if i.gene1()==a and i.sex()==sex])
     def sex_freq_a1(self,a,sex):
-        return self.sex_num_a1(a,sex)/self.size()
+        return self.sex_num_a1(a,sex)/self.num_sex(sex)
         
     def create(self,org):
         self._content = self._content + [org]
+    def remove(self,org):
+        self._content.remove(org)
     def populate(self,sex,gene1,n):
         for x in range(n):
             self.create(org(sex,gene1))
@@ -61,32 +90,43 @@ class pop:
     
         kpm = 2*self.num_sex('f')/self.num_sex('m')
         
-        #males each male gene generates
-        mpa = [(a*kpm*self.sex_num_a1(a,'m'),a) for a in self.gene1pool()]
+        #males each male gene a generates
+        mpa = [(a,a*kpm*self.sex_num_a1(a,'m')) for a in self.gene1pool()]
+        #print(mpa)
         #of which inherit a
-        mpa_inha = [(i[0]/2,i[1]) for i in mpa]
+        mpa_inha = [(['m',i[0]],i[1]/2) for i in mpa]
+        #print(mpa_inha)
         #of which inherit b, the female gene
-        mpa_inhb = [(sum([i[0]*self.sex_freq_a1(b,'f') for i  in mpa_inha]),b) for b in self.gene1pool()]
+        mpa_inhb = [(['m',b],sum([i[1]*self.sex_freq_a1(b,'f') for i  in mpa_inha])) for b in self.gene1pool()]
+        #print(mpa_inhb)
         
         #females each male gene generates
-        fpa = [((1-a)*kpm*self.sex_num_a1(a,'m'),a) for a in self.gene1pool()]
+        fpa = [(a,(1-a)*kpm*self.sex_num_a1(a,'m')) for a in self.gene1pool()]
+        #print(fpa)
         #of which inherit a
-        fpa_inha = [(i[0]/2,i[1]) for i in fpa]
+        fpa_inha = [(['f',i[0]],i[1]/2) for i in fpa]
+        #print(fpa_inha)
         #of which inherit b, the female gene
-        fpa_inhb = [(sum([i[0]*self.sex_freq_a1(b,'f') for i  in fpa_inha]),b) for b in self.gene1pool()]
+        fpa_inhb = [(['f',b],sum([i[1]*self.sex_freq_a1(b,'f') for i  in fpa_inha])) for b in self.gene1pool()]
+        #print(fpa_inhb)
         
-        l1 = [(round(i[0]),i[1]) for i in mpa_inha]
-        l2 = [(round(i[0]),i[1]) for i in mpa_inhb]
-        l3 = [(round(i[0]),i[1]) for i in fpa_inha]
-        l4 = [(round(i[0]),i[1]) for i in fpa_inhb]
+        prob = mpa_inha+mpa_inhb+fpa_inha+fpa_inhb
         
-        lists = [(l1,'m'),(l2,'m'),(l3,'f'),(l4,'f')]
+        for x in range(int(kpm*self.num_sex('m'))):
+            sex,gene1=w_choice(prob)
+            self.create(org(sex,gene1))
+            
+    def kill(self):
         
-        for l in lists:
-            for i in l[0]:
-                for x in range(int(i[0])):
-                    self.create(org(l[1],i[1]))
+        odds_dead = [(i,sigmoid(i.age())) for i in self._content]
         
+        for i in odds_dead:
+            randex(self.remove,i[0],i[1])
+            
+    def evolve(self,gens):
+        for x in range(gens):
+            self.breed()
+            self.kill()
         
         
         
